1f1183e40d02cb7baf3f539facfcfc1169faa26c
Ximena Valenzuela <xduarte@uabc.edu.mx>
2022-05-27 01:33:54 -0700

    Agregar archivos de proyecto.

---
+++ for/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte i, numAlumnos;
+            double calificacion, sumaCalif = 0, promedio;
+            Console.Write("Ingresa el numero de alumnos: ");
+            numAlumnos = Convert.ToByte(Console.ReadLine());
+
+            for (i = 1; i <= numAlumnos; i++)
+            {
+                Console.Write("Ingresa la calificacion: ");
+                calificacion = Convert.ToDouble(Console.ReadLine());
+                sumaCalif += calificacion;
+            }
+            promedio = sumaCalif / numAlumnos;
+            Console.WriteLine("El promedio es: {0}", promedio);
+            Console.ReadKey();
+        }
+    }
+}
